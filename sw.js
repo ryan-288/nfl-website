825b49bd9d7680212590a7ae440848e325345e81
ryan-288 <158698198+ryan-288@users.noreply.github.com>
2025-08-21 18:49:46 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'sports-scores-v5';
+const CACHE_NAME = 'sports-scores-v6';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/styles.css?v=20250821&t=202508211800',
+  '/styles.css?v=20250821&t=202508212000',
   '/script.js',
   '/manifest.json'
 ];
